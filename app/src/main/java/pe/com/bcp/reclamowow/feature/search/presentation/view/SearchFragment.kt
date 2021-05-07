5e69cb6c3ffc6a607a1b09037c44882e6399b173
Moises Almeyda <moisesalmeyda@bcp.com.pe>
2021-05-07 16:59:44 -0500

    Add redirect Search to Success

--- app/src/main/java/pe/com/bcp/reclamowow/feature/search/presentation/view/SearchFragment.kt
+++ app/src/main/java/pe/com/bcp/reclamowow/feature/search/presentation/view/SearchFragment.kt
@@ -1,11 +1,14 @@
 package pe.com.bcp.reclamowow.feature.search.presentation.view
 
 import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
 import org.koin.android.viewmodel.ext.android.viewModel
+import pe.com.bcp.reclamowow.R
 import pe.com.bcp.reclamowow.common.Constants
 import pe.com.bcp.reclamowow.core.base.BaseFragment
 import pe.com.bcp.reclamowow.core.base.BaseViewModel
 import pe.com.bcp.reclamowow.databinding.FragmentSearchBinding
+import pe.com.bcp.reclamowow.feature.register.domain.model.RegisterSuccessModel
 import pe.com.bcp.reclamowow.feature.search.presentation.viewmodel.SearchViewModel
 
 
@@ -24,7 +27,18 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>() {
     override fun init() {
         val typeDocumentArrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, Constants.typeDocumentos)
         bindingView.spinnerTypeDocument.adapter = typeDocumentArrayAdapter
+
+        bindingView.registerClaimButton3.setOnClickListener {
+            goToSuccess()
+        }
+
     }
 
+    private fun goToSuccess() {
+        val navigate = SearchFragmentDirections.actionSearchFragmentToRegisterSuccessFragment(
+            RegisterSuccessModel(codeClaim = "001231",dateEnd = "20/20/2021")
+        )
+        findNavController().navigate(navigate)
+    }
 
 }
